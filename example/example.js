c28b031a1820d6fc04ab14cf097512f0df57fee9
IndexOverflow <post@knutgaute.com>
2015-09-23 11:15:03 +0200

    Made template compiling more solid, should avoid interpolate errors. Cleaned up examples. Added support for filters in messages.

--- example/example.js
+++ example/example.js
@@ -1,6 +1,7 @@
 angular
 .module('exampleOrange', ['orangeNotification'])
 .config(configure)
+.filter('test', testFilter)
 .controller('ExampleCtrl', ExampleCtrl);
 
 //////////
@@ -8,13 +9,34 @@ angular
 configure.$inject = ['orangeNotificationProvider'];
 
 function configure(orangeNotificationProvider){
-  orangeNotificationProvider.setDirectiveTemplateUrl('example-implementation.html');
+  orangeNotificationProvider.setDirectiveTemplateUrl('/example/example-implementation.html');
+  orangeNotificationProvider.setNotificationExpiraiton(5);
+  orangeNotificationProvider.setFilter('test');
 }
 
 //////////
 
-ExampleCtrl.$inject = ['orangeNotification'];
+function testFilter() {
+  return function(input){
+    return 'Message: ' + input;
+  }
+}
 
-function ExampleCtrl(ns) {
-  ns.info('Hello World!');
+//////////
+
+ExampleCtrl.$inject = ['orangeNotification', '$timeout'];
+
+function ExampleCtrl(ns, $timeout) {
+  ns.info('I am an info message');
+  ns.warning('Darn, I have to warn you about something!');
+  ns.error('Oh no, something didn\'t work');
+  ns.success('Puh, crisis averted');
+
+  var promise = $timeout(function(){
+    // ... waiting
+  }, 10000, false);
+
+  ns.wait('I\'ll stay here until my promise is resolved ...', promise);
+
+
 }
